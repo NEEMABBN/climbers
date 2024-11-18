2d6f24915537ef1fc47cc57f9b37bf34a73ac488
nima <nima.babanezhad.arimi@gmail.com>
2024-11-18 12:29:06 +0330

    Completed Navbar

---
+++ src/Components/NavbarItem.jsx
@@ -0,0 +1,125 @@
+import React, { useEffect, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
+export default function NavbarItem() {
+  const location = useLocation();
+  const [underlineStyle, setUnderlineStyle] = useState({});
+
+  useEffect(() => {
+    const activeLink = document.querySelector(".nav-link.active");
+    if (activeLink) {
+      const { offsetLeft, offsetWidth } = activeLink;
+      setUnderlineStyle({
+        left: `${offsetLeft}px`,
+        width: `${offsetWidth}px`,
+      });
+    }
+  }, [location]);
+
+  return (
+    <div className="w-full border-solid border-y-[1px] border-Borders">
+      <ul className="container mx-auto flex items-center justify-start relative gap-3 py-2">
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `${
+                isActive ? "text-Primary nav-link active" : "nav-link nav-link"
+              }`
+            }
+          >
+            خانه
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Tours"
+            className={({ isActive }) =>
+              `${
+                isActive ? "text-Primary nav-link active" : "nav-link nav-link"
+              }`
+            }
+          >
+            تورها
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/TouristAttractions"
+            className={({ isActive }) =>
+              `${
+                isActive ? "text-Primary nav-link active" : "nav-link nav-link"
+              }`
+            }
+          >
+            نقشه
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Agencies"
+            className={({ isActive }) =>
+              `${
+                isActive ? "text-Primary nav-link active" : "nav-link nav-link"
+              }`
+            }
+          >
+            اماکن
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Reports"
+            className={({ isActive }) =>
+              `${
+                isActive ? "text-Primary nav-link active" : "nav-link nav-link"
+              }`
+            }
+          >
+            گزارش برنامه
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Contact"
+            className={({ isActive }) =>
+              `${
+                isActive ? "text-Primary nav-link active" : "nav-link nav-link"
+              }`
+            }
+          >
+            تیم‌ها
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/somepage"
+            className={({ isActive }) =>
+              `${
+                isActive ? "text-Primary nav-link active" : "nav-link nav-link"
+              }`
+            }
+          >
+            فروشگاه
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Blogs"
+            className={({ isActive }) =>
+              `${
+                isActive ? "text-Primary nav-link active" : "nav-link nav-link"
+              }`
+            }
+          >
+            بلاگ
+          </NavLink>
+        </li>
+        <div
+          className="absolute bottom-0 h-1.5 rounded-full bg-Primary transition-all duration-300"
+          style={underlineStyle}
+        ></div>
+      </ul>
+    </div>
+  );
+}
--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { FiSearch } from "react-icons/fi";
+import Images from "../Setting/Images";
+import NavbarItem from "../Components/NavbarItem";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="w-full flex flex-col items-center">
+      <div className="container mx-auto flex items-center justify-between py-4">
+        <div className="flex items-center gap-5">
+          <img src={Images.Logo} alt="" className="w-[117px]" />
+          <div className="flex items-center border-solid border-2 border-Borders rounded-2xl bg-white w-[400px] gap-3 py-2 pr-2 pl-7">
+            <FiSearch className="text-3xl text-Primary rotate-90" />
+            <input
+              type="text"
+              placeholder="جستجوی اماکن گردشگری، تورها، ..."
+              className="w-full outline-none rounded-l-2xl py-1"
+            />
+          </div>
+        </div>
+        <button className="text-white bg-Primary px-5 py-2 rounded-2xl">
+          ورود / ثبت‌نام
+        </button>
+      </div>
+      <NavbarItem />
+    </div>
+  );
 }
