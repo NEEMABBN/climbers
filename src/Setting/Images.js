b801c0d46973a5316268b82b5b5cee06025df59b
nima <nima.babanezhad.arimi@gmail.com>
2024-11-18 11:10:32 +0330

    Add Font & Colors

---
+++ src/Setting/Images.js
@@ -0,0 +1,7 @@
+import Logo from "/src/assets/Images/Logo.png"
+
+const Images = {
+    Logo: Logo,
+}
+
+export default Images
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Primary: "#0E9A8A",
+        Secoundray: "#0F1D1A",
+        Titles: "#2C3639",
+        Borders: "#EEEEEE",
+        Disable: "#B0AFA9",
+      }
+    },
   },
   plugins: [],
 }
-
