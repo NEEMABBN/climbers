c86008b8001dcec9fc2f0fd2967edaab9069ccd9
nima <nima.babanezhad.arimi@gmail.com>
2024-11-26 17:55:48 +0330

    Fixed Navbar Pages Name In Moblie Responsive

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import Images from "../Setting/Images";
 import NavbarItem from "../Components/NavbarItem";
 import { IoMenu } from "react-icons/io5";
 import SideMenu from "./SideMenu";
-import { useLocation } from "react-router-dom";
+import { matchPath, useLocation } from "react-router-dom";
 
 export default function Navbar() {
   const location = useLocation();
@@ -12,6 +12,50 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+  useEffect(() => {
+    setIsOpen(false);
+  }, [location.pathname]);
+
+  const getPageName = () => {
+    const matchTours = matchPath("/tours/:id", location.pathname);
+
+    if (matchTours) {
+      return "تورها";
+    }
+    switch (location.pathname) {
+      case "/":
+        return (
+          <img
+            src={Images.LogoMobile}
+            alt="Climbers Logo"
+            className="w-[117px] md:hidden block"
+          />
+        );
+      case "/Tours":
+      case "/Tours/Search":
+        return "تورها";
+      case "/TouristAttractions":
+        return "نقشه";
+      case "/Reports":
+        return "گزارش برنامه";
+      case "/Agencies":
+        return "اماکن";
+      case "/Blogs":
+        return "بلاگ";
+      case "/Contact":
+        return "تیم‌ها";
+      case "/SomePage":
+        return "فروشگاه";
+      default:
+        return (
+          <img
+            src={Images.LogoMobile}
+            alt="Climbers Logo"
+            className="w-[117px] md:hidden block"
+          />
+        );
+    }
   };
 
   return (
@@ -26,11 +70,9 @@ export default function Navbar() {
             alt="Climbers Logo"
             className="w-[117px] md:block hidden"
           />
-          <img
-            src={Images.LogoMobile}
-            alt="Climbers Logo"
-            className="w-[117px] md:hidden block"
-          />
+          <div className="md:hidden flex text-lg font-bold text-Secoundray">
+            {getPageName()}
+          </div>
           <div className="md:flex hidden items-center border-solid border-2 border-Borders rounded-2xl bg-white w-[400px] gap-3 py-2 pr-2 pl-7">
             <FiSearch className="text-3xl text-Primary rotate-90" />
             <input
