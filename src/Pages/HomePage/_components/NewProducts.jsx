786a8734d532016fd192b55439251a96c365cb75
nima <nima.babanezhad.arimi@gmail.com>
2024-12-12 10:50:08 +0330

    Removed Loop For NewProduct Section Sliders (Home Page)

--- src/Pages/HomePage/_components/NewProducts.jsx
+++ src/Pages/HomePage/_components/NewProducts.jsx
@@ -87,7 +87,6 @@ export default function NewProducts() {
               },
             }}
             dir="rtl"
-            loop={true}
             pagination={{
               clickable: true,
             }}
